cccdc289fc946441c97dd28c6c5c4347c7e19fba
fengliuhuanghe <273730246@qq.com>
2017-05-29 12:59:51 +0800

    @commit code

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+class HelloWorld
+{
+	public void sayHello()
+	{
+		System.out.println("HelloWorld");
+	}
+	
+	public static void main(String[] args)
+	{
+		HelloWorld helloworld = new HelloWorld();
+		helloworld.sayHello();
+	}
+}
